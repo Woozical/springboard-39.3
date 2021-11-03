1331c9d7b4d7ff0f1a1452ecb22e14ac327fb494
Mark <mark@example.dev>
2021-11-02 20:51:20 -0700

    styling

--- react-pokedex/src/PokeCard.js
+++ react-pokedex/src/PokeCard.js
@@ -1,12 +1,17 @@
+import './styles/PokeCard.css';
+
 const PokeCard = (props) => {
     const imgURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${props.id}.png`;
     const imgAlt = `Image of ${props.name}`;
     return (
-        <div>
-            <b>{props.name}</b>
-            <img src={imgURL} alt={imgAlt}/>
-            <p>Type: {props.type}</p>
-            <p>EXP: {props.exp}</p>
+        <div className="PokeCard-container">
+            <h3 className="PokeCard-name">{props.name}</h3>
+            <img className="PokeCard-image" src={imgURL} alt={imgAlt}/>
+            <p>
+                Type: {props.type}
+                <br />
+                EXP: {props.exp}
+            </p>
         </div>
     )
 }
